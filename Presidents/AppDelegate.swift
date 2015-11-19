bf002f956579a6c9daf73d2b450e26baf46f9c16
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2015-11-19 12:18:20 -0500

    Created new master-detail application and copied Presidents.plist file. This is the last commit before modifying the sample project.

--- Presidents/AppDelegate.swift
+++ Presidents/AppDelegate.swift
@@ -17,8 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         let splitViewController = self.window!.rootViewController as! UISplitViewController
-        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
-        navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
+        let navigationController = splitViewController.viewControllers.last as! UINavigationController
+        // assigns the diplayModeButtonItem of the split view controller to the navigation bar of the detail view controller
+       navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
         splitViewController.delegate = self
         return true
     }
